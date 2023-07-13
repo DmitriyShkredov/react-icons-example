5fd94b4f5c45011dfcb5e27e83d1f6ad4ef6beb2
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-07-13 21:36:30 +0300

    feat: add react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import "./App.css";
+import { AiFillCalculator } from "react-icons/ai";
+import { BsFillArrowUpLeftSquareFill } from "react-icons/bs";
+import { BiArea } from "react-icons/bi";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul className="list">
+        <li className="item">
+          <AiFillCalculator className="icon" />
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique?
+          </span>
+        </li>
+        <li className="item">
+          <BsFillArrowUpLeftSquareFill className="icon" />
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique?
+          </span>
+        </li>
+        <li className="item">
+          <BiArea className="icon" />
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique?
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
